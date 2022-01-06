8324dbdf768ede69d283bd06384f920d6e4fde65
fainnerramirez <fainnerramirez@gmail.com>
2022-01-06 12:11:55 -0500

    update state

--- src/components/GifApp.jsx
+++ src/components/GifApp.jsx
@@ -12,11 +12,15 @@ const GifApp = () => {
       <AddCategory setCategory={setCategory} />
       <hr />
       <div>
-        <>
-          {categorys.map((category, i) => {
-            return <GifGrid key={category} category={category} />;
-          })}
-        </>
+        {categorys.length === 0 ? (
+          <h3 className="text-center p-5">Busca cuaquier cosa</h3>
+        ) : (
+          <>
+            {categorys.map((category, i) => {
+              return <GifGrid key={category} category={category} />;
+            })}
+          </>
+        )}
       </div>
     </div>
   );
